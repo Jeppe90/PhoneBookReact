0e8f1e5275b1fe59263b0b6f46418f144509c709
Jesper <Jeppe_90@hotmail.com>
2020-04-22 11:58:06 +0200

    signup method creating a user in firebase method implemented

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { signIn } from '../actions'
+import { Redirect } from 'react-router-dom'
 
 class SignIn extends Component {
     state = {
@@ -19,7 +20,8 @@ class SignIn extends Component {
         console.log(this.props.signIn(this.state));
     }
     render() {
-        const { authError } = this.props;
+        const { authError, auth } = this.props;
+        if(auth.uid) return <Redirect to='/'/>
 
         return (
             <div>
@@ -52,7 +54,8 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state) => {
     return{
-        authError: state.auth.authError
+        authError: state.auth.authError,
+        auth: state.firebase.auth
     }
 }
 
--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -1,50 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../actions";
 
 class SignUp extends Component {
-    state = {
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: ''
-    }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id] : e.target.value
-        })
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state);
-    }
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <h5>Sign in</h5>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange}></input>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange}></input>
-                    </div>
-                    <div>
-                        <label htmlFor="firstName">First name</label>
-                        <input type="text" id="firstName" onChange={this.handleChange}></input>
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Last name</label>
-                        <input type="text" id="lastName" onChange={this.handleChange}></input>
-                    </div>
-                    <div>
-                        <button>
-                            Sign up
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  state = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signUp(this.state);
+  };
+  render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to="/" />;
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h5>Sign in</h5>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              id="firstName"
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              id="lastName"
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div>
+            <button>Sign up</button>
+            <div>{authError ? <p>{authError}</p> : null}</div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-export default SignUp
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
